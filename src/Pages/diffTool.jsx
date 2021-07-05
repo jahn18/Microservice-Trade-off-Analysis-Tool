354849e5676588fc3d66a5bdca76af840adfd8d7
jahn18 <john.ahn.es@gmail.com>
2021-07-05 13:42:06 -0700

    Added table configuration for metrics

--- src/Pages/diffTool.jsx
+++ src/Pages/diffTool.jsx
@@ -7,7 +7,12 @@ import 'bootstrap/dist/js/bootstrap.js';
 import Paper from "@material-ui/core/Paper";
 import Tab from "@material-ui/core/Tab";
 import Tabs from "@material-ui/core/Tabs";
-import { unstable_renderSubtreeIntoContainer } from "react-dom";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 // import compoundDragAndDrop from 'cytoscape-compound-drag-and-drop';
 
@@ -32,6 +37,7 @@ class DiffTool extends React.Component {
             graphData: json_graph_data,
             diffGraph: diffGraph,
             selectedRelationshipType: 'static',
+            // I have to change this when the two decompositions have a different number of partitions.
             num_of_partitions: num_of_partitions,
             data: {
                 common_elements: common,
@@ -42,6 +48,46 @@ class DiffTool extends React.Component {
 
         this.changeRelationshipType = this.changeRelationshipType.bind(this);
         this.getEdgeDependencies = this.getEdgeDependencies.bind(this);
+    };
+
+    calculateNormalizedTurboMQ = (diffGraph, common_elements, graph_num) => {
+        const {num_of_partitions, graphData} = this.state;
+        let CF_0 = 0.0;
+        let CF_1 = 0.0;
+        for(let i = 0; i < num_of_partitions; i++) {
+            /* 
+                Later change this so that graph_num is the actual number so it is "graph_${graph_num}_diff" when we have
+                multiple partitions. 
+            */
+            let partition_classes = (graph_num == 1) ? [].concat(diffGraph[i].common, diffGraph[i].graph_one_diff) : 
+                                        [].concat(diffGraph[i].common, diffGraph[i].graph_two_diff); 
+            let internal_edges = 0.0;
+            let external_edges = 0.0; 
+            let edge_graph = graphData.data.static_graph.links;
+            for(let j = 0; j < edge_graph.length; j++) {
+                let edge = edge_graph[j];
+                if(partition_classes.includes(edge.source) && partition_classes.includes(edge.target)) {
+                    internal_edges += parseFloat(edge.weight);
+                } else if (partition_classes.includes(edge.source) || partition_classes.includes(edge.target)) {
+                    external_edges += parseFloat(edge.weight); 
+                }
+            }
+            CF_0 += (internal_edges != 0) ? ((2*internal_edges) / ( (2*internal_edges) + external_edges)) : 0; 
+
+            internal_edges = 0.0;
+            external_edges = 0.0; 
+            edge_graph = graphData.data.class_name_graph.links;
+            for(let j = 0; j < edge_graph.length; j++) {
+                let edge = edge_graph[j];
+                if(partition_classes.includes(edge.source) && partition_classes.includes(edge.target)) {
+                    internal_edges += parseFloat(edge.weight);
+                } else if (partition_classes.includes(edge.source) || partition_classes.includes(edge.target)) {
+                    external_edges += parseFloat(edge.weight); 
+                }
+            }
+            CF_1 += (internal_edges != 0) ? ((2*internal_edges) / ( (2*internal_edges) + external_edges)) : 0; 
+        }
+        return [(CF_0 / num_of_partitions) * 100, (CF_1 / num_of_partitions) * 100];
     };
 
     // When the user clicks on a tab it changes the relationship type.
@@ -120,10 +166,9 @@ class DiffTool extends React.Component {
         return element_list;
     };
 
-    highlightEdge = () => {
+    // highlightEdge = () => {
         
-    }
-
+    // }
 
     render() {
         const {num_of_partitions, diffGraph} = this.state;
@@ -151,6 +196,9 @@ class DiffTool extends React.Component {
             });
             elements = [].concat(elements, this.setUpPartitions(diffGraph[i].common, i, 0, true));
         }
+
+        let static_turboMQ = this.calculateNormalizedTurboMQ(diffGraph, common_elements, 1);
+        let class_name_turboMQ = this.calculateNormalizedTurboMQ(diffGraph, common_elements, 2);
 
         return (
             <div>
@@ -318,6 +366,44 @@ class DiffTool extends React.Component {
 
                     }}
                 />
+                <TableContainer 
+                component={Paper} 
+                style={
+                        {
+                            width: '25%',
+                            border: '1px solid grey',
+                            'left': '74%',
+                            'margin-top': '1%',
+                            position: 'fixed',
+                        }
+                    } 
+                size="small">
+                    <Table aria-label="simple table">
+                        <TableHead>
+                        <TableRow>
+                            <TableCell>Coupling & Cohesion (0-100%)</TableCell>
+                            <TableCell align="center">Static Dependencies</TableCell>
+                            <TableCell align="center">Class Name Dependencies</TableCell>
+                        </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow key={'static'}>
+                            <TableCell component="th" scope="row">
+                                {'STATIC'}
+                            </TableCell>
+                            <TableCell align="center">{static_turboMQ[0].toFixed(2)}</TableCell>
+                            <TableCell align="center">{static_turboMQ[1].toFixed(2)}</TableCell>
+                            </TableRow>
+                            <TableRow key={'class name'}>
+                            <TableCell component="th" scope="row">
+                                {'CLASS NAME'}
+                            </TableCell>
+                            <TableCell align="center">{class_name_turboMQ[1].toFixed(2)}</TableCell>
+                            <TableCell align="center">{class_name_turboMQ[0].toFixed(2)}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </div>
         );
     }
